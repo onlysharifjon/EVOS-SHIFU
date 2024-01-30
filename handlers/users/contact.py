c08977a2bfe996b9c2386326d23c10cceec80cdb
onlysharifjon <mominovsharif12@gmail.com>
2024-01-30 14:38:07 +0500

    contact database

--- handlers/users/contact.py
+++ handlers/users/contact.py
@@ -1,15 +1,21 @@
 from loader import dp
 from aiogram import types
 from keyboards.default.userbuttons import joylashuv, start_button
+from .start import cursor, connect
 
 
 @dp.message_handler(content_types=types.ContentTypes.CONTACT)
 async def contact_ga_javob_ber(message: types.Message):
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS contact_table(phone_number TEXT,last_name TEXT,user_id INTEGER,username TEXT)')
     a = message.contact
     nomeri = a['phone_number']
     ismi = a['first_name']
     id_odam = a['user_id']
-    print(a)
+    username = message.from_user.username
+    print(username)
+    cursor.execute('INSERT INTO contact_table VALUES(?,?,?,?)', (nomeri, ismi, message.from_user.id, username))
+    connect.commit()
     await message.answer('Manzilingizni jo`nating', reply_markup=joylashuv)
 
 
