7ef263d3a4dafaf3be7c4da1c3f299e185f0a9a6
m1ralisherdev <alishermirmaxmudov3@gmail.com>
2024-02-08 13:56:28 +0500

    Alisher

--- test.py
+++ test.py
@@ -7,3 +7,4 @@ cursor.execute("CREATE TABLE IF NOT EXISTS mahsulotlar(name TEXT,price INTEGR,im
 cursor.execute('INSERT INTO mahsulotlar VALUES (?,?,?,?)',
                ("Lavash mol go'sht", 26000, "Lavash_mol_gosht.jpg", 'lavashlar'))
 connect.commit()
+#
