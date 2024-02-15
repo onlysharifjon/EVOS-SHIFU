1240460ae16e779ed425375c291169815ea93e9a
Sharifjon Mo`minov <sh.muminov@tuit.uz>
2024-02-15 15:00:12 +0500

    dars

--- handlers/users/mahsulotlar.py
+++ handlers/users/mahsulotlar.py
@@ -2,12 +2,20 @@ from loader import dp
 from states.state_aiogram import Bolimlar
 from aiogram import types
 import sqlite3
-
-connect = sqlite3.connect('evos_database.db')
+from keyboards.inline.mahsulotlar_inline import mahsulot_inline
+connect = sqlite3.connect('C:/Users/momin/PycharmProjects/EVOS-SHIFU/evos_.database.db')
 cursor = connect.cursor()
 
 
 @dp.message_handler(state=Bolimlar.setlar)
 async def setlar_funksiyasi(message: types.Message):
-    cursor.execute("CREATE TABLE IF NOT EXISTS mahsulotlar(name TEXT,price INTEGR,image TEXT,category TEXT)")
-    print(message.text)
+    print(type(message.text))
+    mahsulot_malumotlari = cursor.execute(f"SELECT * FROM mahsulotlar WHERE name=?", (message.text,)).fetchall()
+    print(mahsulot_malumotlari)
+    # test ma`lumot [('FitCombo', 25000, 'Fit_Combo.jpg', 'setlar')]
+    txt = f'''
+😋Mahsulot nomi: {mahsulot_malumotlari[0][0]}
+💸Mahsulot narxi: {mahsulot_malumotlari[0][1]}
+📃Mahsulot kategoriyasi: {mahsulot_malumotlari[0][3]}
+    '''
+    await message.answer_photo(open(f'images/product_image/{mahsulot_malumotlari[0][2]}', 'rb'), caption=txt,reply_markup=mahsulot_inline)
--- test.py
+++ test.py
@@ -3,38 +3,12 @@ import sqlite3
 connect = sqlite3.connect('evos_.database.db')
 cursor = connect.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS mahsulotlar(name TEXT,price INTEGR,image TEXT,category TEXT)")
-#
-#
-# #
-# a = ["Shaurma qalampir mol go'sht;22000;Shaurma_qalampir_mol_gosht.jpg",
-#      "Shaurma tovuq go'sht;24000;Shaurma_tovuq_gosht.jpg",
-#      "Shaurma qalampir tovuq go'sht;26000;Shaurma_qalampir_tovuq_gosht.jpg",
-#      "Shaurma mol go'sht;24000;Shaurma_mol_gosht.jpg",
-#      "Gamburger;22000;Gamburger.jpg",
-#      "Double burger;33000;Double_burger.jpg",
-#      "Cheese burger;24000;Cheese_burger.jpg",
-#      "Double cheese;37000;Double_cheese.jpg",
-#      ]
-# mahsulot_nomi = ['Combo Plus Isituvchan (Qora choy)', 'FitCombo', "Iftar kofte grill mol go'shtidan",
-#                  "Donar boks  mol go'shtidan", "Donar boks tovuq go'shtidan", 'COMBO+', "Iftar strips tovuq go'shtidan",
-#                  'Kids COMBO', 'Mol goʼshtidan qalampir lavash', 'Tovuq goʼshtli qalampir lavash',
-#                  "Mol go'shtidan pishloqli lavash Standard", "Lavash cheese tovuq go'sht Standart", 'FITTER',
-#                  "Lavash tovuq go'sht", "Lavash mol go'sht"]
-# narxlar = [28000, 25000, 27000, 24000, 28000, 25000, 24000, 24000, 26000, 24000, 29000, 29000, 22000, 24000, 26000]
-#
-# rasmlar = ['ComboPlusIsituvchan(Qora choy).jpg', 'Fit_Combo.jpg', 'Iftar_kofte_grill_mol_goshtidan.jpg',
-#            'Donar_boks_mol_goshtidan.jpg', 'Donar_boks_tovuq_goshtidan.jpg', 'COMBO+.jpg',
-#            'Iftar_strips_tovuq_goshtidan.jpg', 'Kids_COMBO.jpg', 'Mol_goshtidan_qalampir_lavash.jpg',
-#            'Tovuq_goshtli_qalampir_lavash.jpg', 'Mol_goshtidan_pishloqli_lavash_standard.jpg',
-#            'Lavash_cheese_tovuq_gosht_standart.jpg', 'FITTER.jpg', 'Lavash_tovuq_gosht.jpg', 'Lavash_mol_gosht.jpg']
-#
-# bolimlar = ['setlar', 'setlar', 'setlar', 'setlar', 'setlar', 'setlar', 'setlar', 'setlar', 'lavashlar', 'lavashlar',
-#             'lavashlar', 'lavashlar', 'lavashlar', 'lavashlar', 'lavashlar']
-
-
-cursor.execute('INSERT INTO mahsulotlar VALUES (?,?,?,?)',
-               (f"Fri", 14000, f"Fri.jpg", f'garnirlar'))
-
-
-connect.commit()
+# cursor.execute("CREATE TABLE IF NOT EXISTS mahsulotlar(name TEXT,price INTEGR,image TEXT,category TEXT)")
+# cursor.execute("DELETE FROM mahsulotlar WHERE name='Gamburger'")
+# connect.commit()
+# connect.commit()
+# Gamburger,22000,Gamburger.jpg,burgerlar
+# cursor.execute('INSERT INTO mahsulotlar VALUES (?,?,?,?)', ('Gamburger', 22000, 'Gamburger.jpg', 'burgerlar'))
+# connect.commit()
+# cursor.execute(f"UPDATE mahsulotlar SET image = 'Cheese_burger.jpg' WHERE image=';Cheese_burger.jpg'");
+# connect.commit()
