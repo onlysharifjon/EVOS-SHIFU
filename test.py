a4e7244ff2520145a725d52f6e0218aaaa4a8c61
onlysharifjon <mominovsharif12@gmail.com>
2024-02-27 09:46:59 +0500

    Buyurtmalar tarixi part 2

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import sqlite3
-
-connect = sqlite3.connect('C:/Users/Sharifjon/PycharmProjects/EVOS-SHIFU/evos_.database.db')
-cursor = connect.cursor()
-
-cursor.execute("CREATE TABLE IF NOT EXISTS buyurtma_tarixi(id INTEGER PRIMARY KEY AUTOINCREMENT, buyurtma TEXT,buyurtmachi_id)")
+# import sqlite3
+#
+# connect = sqlite3.connect('C:/Users/Sharifjon/PycharmProjects/EVOS-SHIFU/evos_.database.db')
+# cursor = connect.cursor()
+#
+# cursor.execute("CREATE TABLE IF NOT EXISTS buyurtma_tarixi(id INTEGER PRIMARY KEY AUTOINCREMENT, buyurtma TEXT,buyurtmachi_id)")
+# #
